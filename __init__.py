a00a657df866604cd43c902681d797da6dae0414
武建明 <wjmwjmwb@yeah.net>
2017-08-04 16:22:44 +0800

    add qiniu_uploadtoken.api

--- __init__.py
+++ __init__.py
@@ -1,19 +1,33 @@
 from macpath import join
 
+import qiniu
 from flask import Flask,request,jsonify
 from haishoku.haishoku import Haishoku
-# from qiniu import Auth
+from qiniu import Auth
 app = Flask(__name__)
 
 @app.route("/")
 def root():
     return "success"
 
-@app.route("/getuploadtoken")
+@app.route("/uploadtoken",methods=['get'])
 def get_uploadtoken():
-    return "231313123"
+    try:
+        q = qiniu.Auth("G3meMJe5tIidwGR271mDStmwp900AFRIvQ8S6d-y", "GyPLkmlb3ORbFZjvF42s_Uo-q14FV-5hIpFoSNeH")
+        token = q.upload_token("photo")
+        print(token)
+        return jsonify({
+            "code":200,
+            "token":token
+        })
+    except Exception as err:
+        return jsonify(
+            {
+                "code":501
+            }
+        )
 
-@app.route("/getcolor",methods=['post'])
+@app.route("/color",methods=['post'])
 def get_color():
 
     try:
